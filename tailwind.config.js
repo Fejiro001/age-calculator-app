b056053d6100bad1325e7f4e06f783067207e5b1
Abere Fejiro <aberefejiro@gmail.com>
2024-04-09 23:27:43 +0100

    major changes still need to be done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js}", "index.html"],
   theme: {
+    screens: {
+      lg: "820px",
+    },
     extend: {
       fontFamily: {
         primary: ["Poppins", "sans-serif"],
+      },
+      fontSize: {
+        large: "clamp(3.75rem,6rem,5.5rem)"
       },
       colors: {
         primary: "hsl(259, 100%, 65%)",
@@ -13,9 +19,6 @@ module.exports = {
         "light-grey": "hsl(0, 0%, 86%)",
         "smokey-grey": "hsl(0, 1%, 44%)",
         "off-black": "hsl(0, 0%, 8%)",
-      },
-      screens: {
-        lg: "1024px",
       },
     },
   },
