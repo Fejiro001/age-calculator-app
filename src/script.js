87d9057efc196522e04d27de48553f074c19f50b
Abere Fejiro <aberefejiro@gmail.com>
2024-04-20 22:29:44 +0100

    grabbed eleements to be used, added code for empty field

--- src/script.js
+++ src/script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+/* 
+- Display day, month and year from the form
+- Make sure the date is valid and display validation errors if:
+  - Any field is empty when the form is submitted (This field is required)
+  - The day number is not between 1-31 (Must be a valid date)
+  - The month number is not between 1-12 (Must be a valid month)
+  - The year number is in the future (Must be in the past)
+  - The date is invalid e.g. 31/04/1991 (there are 30 days in April) (Must be a valid date)
+- Don't display date if there is any validation error, one or more
+- Change input border and label color if there is any invalid input
+*/
+
+const formLabels = document.querySelectorAll("#label");
+const formInputs = document.querySelectorAll(".input-field");
+const inputErrors = document.querySelectorAll(".error");
+const calculateAgeButton = document.getElementById("calculate-btn");
+const yourAge = document.querySelectorAll("#age");
+
+calculateAgeButton.addEventListener("click", function () {
+  for (let i = 0; i < formInputs.length; i++) {
+    if (!formInputs[i].value) {
+      // console.log("empty");
+      for (let j = 0; j < inputErrors.length; j++) {
+        inputErrors[i].innerHTML = "This field is required";
+      }
+    }
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,15 @@
 module.exports = {
   content: ["./src/script.js}", "index.html"],
   theme: {
-    screens: {
-      lg: "820px",
-    },
     extend: {
       fontFamily: {
         primary: ["Poppins", "sans-serif"],
       },
       fontSize: {
-        "label-font": "clamp(.9rem,.8vw,2rem)",
-        "input-font": "clamp(1.25rem,2.2vw,3rem)",
-        large: "clamp(3.5rem,8vw,6.5rem)",
-        "small": "clamp(1.25rem,2vw,2.5rem)"
+        "label-font": "clamp(.9rem,1.1vw,2rem)",
+        "input-font": "clamp(1.25rem,2.2vw,2.5rem)",
+        large: "clamp(3.3rem,8vw,6.5rem)",
+        small: "clamp(.7rem,1vw,1.5rem)",
       },
       colors: {
         primary: "hsl(259, 100%, 65%)",
@@ -22,6 +19,9 @@ module.exports = {
         "light-grey": "hsl(0, 0%, 86%)",
         "smokey-grey": "hsl(0, 1%, 44%)",
         "off-black": "hsl(0, 0%, 8%)",
+      },
+      screens: {
+        lg: "820px",
       },
     },
   },
